f37085569c2ea58050cf8c82f93e395e145ab3c7
saurabh <saurabhacharya05@gmail.com>
2024-04-06 18:23:38 +0530

    transaction page is updated

--- transaction.py
+++ transaction.py
@@ -1,31 +1,37 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from database import transaction
 
-def transaction_page():
-    # Function to go back to the home screen
+def transaction_page(login,home,loggedin_user):
     def back():
-        root.destroy()  # Close the current window
+        root.destroy()
+        home(login,loggedin_user)
+    def load_data():
+        transactions = transaction(loggedin_user)
+        # Insert data into the treeview
+        for data in transactions:
+            tree.insert("", tk.END, values=data)
 
     # Create main window
     root = tk.Tk()
-    root.title("Transaction History")  # Set window title
-    root.geometry("925x500+300+200")  # Set window size and position
-    root.configure(bg="#fff")  # Set window background color
-    root.resizable(False, False)  # Disable window resizing
+    root.title("Transaction History")
+    root.geometry("925x500+300+200")
+    root.configure(bg="#fff")
+    root.resizable(False,False)
 
     # Create Treeview widget
-    tree = ttk.Treeview(root, columns=("Date", "Phone Number", "Type", "Amount"), show="headings")
+    tree = ttk.Treeview(root, columns=("Date", "Phone", "Type", "Amount"), show="headings")
 
     # Define column headings
     tree.heading("Date", text="Date")
-    tree.heading("Phone Number", text="Phone Number")
+    tree.heading("Phone", text="Phone")
     tree.heading("Type", text="Type")
     tree.heading("Amount", text="Amount")
 
     # Set column widths
     tree.column("Date", width=100)
-    tree.column("Phone Number", width=150)
+    tree.column("Phone", width=150)
     tree.column("Type", width=100)
     tree.column("Amount", width=100)
 
@@ -40,14 +46,11 @@ def transaction_page():
     tree.configure(yscrollcommand=scrollbar.set)
     scrollbar.pack(side="right", fill="y")
 
-    # Pack the Treeview
+    # Load data into the treeview
+    load_data()
+
+    # Pack the treeview
     tree.pack(fill="both", expand=True)
-
-    # Create a button to go back
-    Button(width=15, pady=7, text="Back", bg="#57a1f8", fg="white", border=0, command=back).place(x=800, y=450)
-
-    
+    Button(width=15,pady=7,text="Submit", bg="#57a1f8",fg="white",border=0,command=back).place(x=800,y=450)
     # Run the application
     root.mainloop()
-
-transaction_page()
